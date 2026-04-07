560dd8d8de832901e0b9b54512c30e4aaafbf821
Arlindo21 <arlindolima307@gmail.com>
2026-04-07 10:35:14 -0300

    Adicionando a logica inicial do jogo  par ou impar

---
+++ jogo.py
@@ -0,0 +1,19 @@
+#criando vaiaveis para jogo
+jogador1= int(input('informe um numero '))
+jogador2= int(input('informe um numero '))
+
+#operadores matematicos
+#somar + ex: 1+1 = 2
+#incrementar +=  ex: idade += 1
+#subtracao - ex: 3-2 = 1
+#descremento -= ex: idade -= 1
+#multiplicacao *
+#divisao /
+
+numero = jogador1 = jogador2
+
+#logica = if else 
+if numero % 2 != 0:
+    print('par ganhou ')
+else:
+    print('par ganhou')
